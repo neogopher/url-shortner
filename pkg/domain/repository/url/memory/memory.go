2d7ac3b0ad24b551b03cd55c2f791b05fc188ba6
Sachin Lobo <sachinslobo@gmail.com>
2022-01-21 15:59:09 +0530

    Implemented Memory Repository functions

--- pkg/domain/repository/url/memory/memory.go
+++ pkg/domain/repository/url/memory/memory.go
@@ -4,12 +4,13 @@ package memory
 import (
 	"sync"
 	"url-shortner/pkg/domain/aggregate"
+	"url-shortner/pkg/domain/repository/url"
 )
 
 // MemoryRepository implements the URL repository.
 type MemoryRepository struct {
 	urls map[string]aggregate.URL
-	sync.Mutex
+	mu   sync.Mutex
 }
 
 // NewRepository is a factory function for creating MemoryRepository.
@@ -19,10 +20,19 @@ func NewMemoryRepository() *MemoryRepository {
 	}
 }
 
-func (mr *MemoryRepository) Add(aggregate.URL) error {
+func (mr *MemoryRepository) Add(url aggregate.URL) error {
+	mr.mu.Lock()
+	mr.urls[url.GetID()] = url
+	mr.mu.Unlock()
+
 	return nil
 }
 
 func (mr *MemoryRepository) Get(shortCode string) (aggregate.URL, error) {
-	return aggregate.URL{}, nil
+	u, ok := mr.urls[shortCode]
+	if !ok {
+		return aggregate.URL{}, url.ErrURLNotFound
+	}
+
+	return u, nil
 }
