26266a7a21fb7695fa4b0aaf4b6ebd129aaa4776
Sachin Lobo <sachinslobo@gmail.com>
2022-01-21 17:03:44 +0530

    Implemented Shortner Service methods

--- pkg/services/shortner.go
+++ pkg/services/shortner.go
@@ -1,7 +1,10 @@
 // Package services contains all services that work with Handlers and repositories to perform an action.
 package services
 
-import "url-shortner/pkg/domain/repository/url"
+import (
+	"url-shortner/pkg/domain/aggregate"
+	"url-shortner/pkg/domain/repository/url"
+)
 
 // ShortnerService shortens given URL and stores it in repository.
 type ShortnerService struct {
@@ -16,9 +19,21 @@ func NewShortnerService(u url.Repository) *ShortnerService {
 }
 
 func (s *ShortnerService) Shorten(fullPath string) (string, error) {
-	return "", nil
+	url, err := aggregate.NewURL(fullPath)
+	if err != nil {
+		return "", err
+	}
+
+	s.urls.Add(url)
+
+	return url.GetID(), nil
 }
 
 func (s *ShortnerService) GetFullPath(shortCode string) (string, error) {
-	return "", nil
+	url, err := s.urls.Get(shortCode)
+	if err != nil {
+		return "", err
+	}
+
+	return url.GetFullPath(), nil
 }
