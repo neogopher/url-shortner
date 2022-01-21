80d12b51c3b468e49d51379bf3cc56437cf651ca
Sachin Lobo <sachinslobo@gmail.com>
2022-01-21 17:16:54 +0530

    Defined HTTP Handler methods

--- pkg/handlers/http.go
+++ pkg/handlers/http.go
@@ -1,7 +1,11 @@
 // Package handlers contains all handlers that call services
 package handlers
 
-import "url-shortner/pkg/services"
+import (
+	"url-shortner/pkg/services"
+
+	"github.com/gin-gonic/gin"
+)
 
 // HTTPHandler interacts with ShortnerService.
 type HTTPHandler struct {
@@ -13,4 +17,14 @@ func NewHTTPHandler(s services.ShortnerService) *HTTPHandler {
 	return &HTTPHandler{
 		service: s,
 	}
+}
+
+// GetShortenedURL accepts a URL as an argument over POST and returns a shortened URL.
+func (h *HTTPHandler) GetShortenedURL(c *gin.Context) {
+
+}
+
+// GetLongURL accepts a shortened URL as an argument over POST and returns the original full URL.
+func (h *HTTPHandler) GetLongURL(c *gin.Context) {
+
 }
