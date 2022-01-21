06870eea2cf5b084567d438e5c2475f28fffdc05
Sachin Lobo <sachinslobo@gmail.com>
2022-01-21 19:45:37 +0530

    Implemented File Repository functions

--- pkg/domain/repository/url/file/file.go
+++ pkg/domain/repository/url/file/file.go
@@ -2,8 +2,12 @@
 package file
 
 import (
+	"fmt"
+	"os"
+	"strings"
 	"sync"
 	"url-shortner/pkg/domain/aggregate"
+	"url-shortner/pkg/domain/repository/url"
 )
 
 // FileRepository implements the URL repository.
@@ -14,18 +18,43 @@ type FileRepository struct {
 }
 
 // NewFileRepository is a factory function for creating FileRepository.
-func NewFileRepository() *FileRepository {
+func NewFileRepository(filename string) *FileRepository {
 	return &FileRepository{
-		urls: make(map[string]aggregate.URL),
+		urls:     make(map[string]aggregate.URL),
+		filename: filename,
 	}
 }
 
 // Add adds URL to repository.
 func (fr *FileRepository) Add(url aggregate.URL) error {
+	_, ok := fr.urls[url.GetID()]
+	if !ok {
+
+		fr.mu.Lock()
+		fr.urls[url.GetID()] = url
+
+		// Additionaly write new links to file.
+		f, err := os.OpenFile(fr.filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+		if err != nil {
+			panic(err)
+		}
+
+		defer f.Close()
+		fmt.Fprintln(f, strings.Join([]string{url.GetID(), url.GetFullPath()}, ","))
+		f.Close()
+
+		fr.mu.Unlock()
+	}
+
 	return nil
 }
 
 // Get gets URL from the repository.
 func (fr *FileRepository) Get(shortCode string) (aggregate.URL, error) {
-	return aggregate.URL{}, nil
+	u, ok := fr.urls[shortCode]
+	if !ok {
+		return aggregate.URL{}, url.ErrURLNotFound
+	}
+
+	return u, nil
 }
